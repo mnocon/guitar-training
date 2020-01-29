de04469ef009cc77049878df6ac494a7e162db92
Marek Nocoń <marek.nocon@ez.no>
2020-01-29 10:59:17 +0100

    Refactored data generation from UI

--- scripts.js
+++ scripts.js
@@ -1,27 +1,34 @@
 $(document).ready(function () {
-    setRandomKey();
-    setRandomStrumming();
-    setRandomProgression();
+    const key = getRandomKey();
+
+    const pattern1 = getRandomStrummingPattern(128, 255);
+    const pattern2 = getRandomStrummingPattern(0, 255);
+    const progression = getRandomProgression(key);
+
+    setRandomKey(key);
+    setRandomStrumming(pattern1, pattern2);
+    setRandomProgression(progression);
 });
 
-function setRandomKey() {
+function getRandomKey() {
     const keys = ['A', 'A#', 'Bb', 'B', 'B', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'Ab'];
-    $("#randomScale").text(randomElement(keys));
+
+    return randomElement(keys);
 }
 
-function setRandomStrumming() {
-    var pattern = getRandomPattern(128, 255);
+function setRandomKey(key) {
+    $("#randomScale").text(key);
+}
 
-    $("#pattern_1_1").text(pattern[0]);
-    $("#pattern_1_2").text(pattern[1]);
-    $("#pattern_1_3").text(pattern[2]);
-    $("#pattern_1_4").text(pattern[3]);
-    $("#pattern_1_5").text(pattern[4]);
-    $("#pattern_1_6").text(pattern[5]);
-    $("#pattern_1_7").text(pattern[6]);
-    $("#pattern_1_8").text(pattern[7]);
-
-    var pattern2 = getRandomPattern(0, 255);
+function setRandomStrumming(pattern1, pattern2) {
+    $("#pattern_1_1").text(pattern1[0]);
+    $("#pattern_1_2").text(pattern1[1]);
+    $("#pattern_1_3").text(pattern1[2]);
+    $("#pattern_1_4").text(pattern1[3]);
+    $("#pattern_1_5").text(pattern1[4]);
+    $("#pattern_1_6").text(pattern1[5]);
+    $("#pattern_1_7").text(pattern1[6]);
+    $("#pattern_1_8").text(pattern1[7]);
 
     $("#pattern_2_1").text(pattern2[0]);
     $("#pattern_2_2").text(pattern2[1]);
@@ -33,22 +40,35 @@ function setRandomStrumming() {
     $("#pattern_2_8").text(pattern2[7]);
 }
 
-function setRandomProgression() {
-    // start each bar with tonic
-    $("#progression_1_1").text('I');
-    $("#progression_2_1").text('I');
+function getRandomProgression(key) {
+    let progression = [];
 
-    // end each bar with IV or V chord
-    $("#progression_1_4").text(randomElement(['IV', 'V']));
-    $("#progression_2_4").text(randomElement(['IV', 'V']));
+    const scaleDegrees = ['ii', 'iii', 'IV', 'V', 'vi']; // skip diminished for now. Skip I to make things more interesting
 
-    // rest is filled randomly
-    const degrees = ['I', 'ii', 'iii', 'IV', 'V', 'vi']; // skip diminished for now
+    // 1 measure: I random random IV/V
+    progression.push('I'); // start with a tonic
+    progression.push(randomElement(scaleDegrees));
+    progression.push(randomElement(scaleDegrees));
+    progression.push(randomElement(['IV', 'V']));
 
-    $("#progression_1_2").text(randomElement(degrees));
-    $("#progression_1_3").text(randomElement(degrees));
-    $("#progression_2_2").text(randomElement(degrees));
-    $("#progression_2_3").text(randomElement(degrees));
+    // 2 measure: I random random IV/V
+    progression.push('I'); // start with a tonic
+    progression.push(randomElement(scaleDegrees));
+    progression.push(randomElement(scaleDegrees));
+    progression.push(randomElement(['IV', 'V']));
+
+    return progression;
+}
+
+function setRandomProgression(progression) {
+    $("#progression_1_1").text(progression[0]);
+    $("#progression_1_2").text(progression[1]);
+    $("#progression_1_3").text(progression[2]);
+    $("#progression_1_4").text(progression[3]);
+    $("#progression_2_1").text(progression[4]);
+    $("#progression_2_2").text(progression[5]);
+    $("#progression_2_3").text(progression[6]);
+    $("#progression_2_4").text(progression[7]);
 }
 
 function randomElement(elements) {
@@ -63,7 +83,7 @@ function toByte(n) {
     return ("000000000" + n.toString(2)).substr(-8)
 }
 
-function getRandomPattern(min, max) {
+function getRandomStrummingPattern(min, max) {
     randomNumber = getRandomInt(min, max);
     byte = toByte(randomNumber);
     newByte = '';
